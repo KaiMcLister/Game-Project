2ea489720c9598a32a4c1ba8c51cdfae0c01f62e
KaiMcLister <71454898+KaiMcLister@users.noreply.github.com>
2020-09-17 09:54:35 -0700

    added cloud class

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,15 +1,15 @@
 //create a variable to hold one ball
 let b;
 let anotherBall;
-let thirdBall;
-let fourthBall;
+let cloud1;
+let cloud2;
 
 function setup() {
   createCanvas(800, 400);
   b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
-  anotherBall = new Ball(200,20,"yellow");
-  thirdBall = new Ball(100,150, "orange");
-  fourthBall = new Ball(200,300, "purple");
+  anotherBall = new Ball(200,20,"green");
+  cloud1 = new Cloud(100,150);
+  cloud2 = new Cloud(200,300);
 }
 
 
@@ -19,10 +19,10 @@ function draw(){
     b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
     anotherBall.drawBall();
     anotherBall.moveBall();
-    thirdBall.drawBall();
-    thirdBall.moveBall();
-    fourthBall.drawBall();
-    fourthBall.moveBall();
+    cloud1.drawCloud();
+    cloud1.moveCloud();
+    cloud2.drawCloud();
+    cloud2.moveCloud();
 
 }
 
@@ -33,55 +33,34 @@ class Ball {
 	constructor(x,y,color){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
-        this.color= color;
+        this.color = color;
 	}
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
     		fill(this.color);
-		    hypercube(this.x,this.y,10,10);
+		    ellipse(this.x,this.y,10,10);
 	}
 	moveBall(){ //update the location of the ball, so it moves across the screen
 		this.x = this.x+2;
 		this.y = this.y+.5;
 	}
 }
-  function hypercube_1(x,y) {
-  push();
-  noFill();
-  line(x+125-140,y+105-140,x+125-140,y+170-140);
-  line(x+105-140,y+125-140,x+170-140,y+125-140);
-  line(x+105-140,y+150-140,x+170-140,y+150-140);
-  line(x+150-140,y+170-140,x+150-140,y+105-140);
-  line(x+150-140,y+105-140,x+105-140,y+150-140);
-  line(x+105-140,y+125-140,x+150-140,y+170-140);
-  line(x+125-140,y+170-140,x+170-140,y+125-140);
-  line(x+170-140,y+150-140,x+125-140,y+105-140);
-  line(x+105-140,y+125-140,x+105-140,y+60-140);
-  line(x+105-140,y+60-140,x+170-140,y+60-140);
-  line(x+170-140,y+60-140,x+170-140,y+125-140);
-  line(x+150-140,y+105-140,x+215-140,y+105-140);
-  line(x+215-140,y+105-140,x+215-140,y+170-140);
-  line(x+215-140,y+170-140,x+150-140,y+170-140);
-  line(x+170-140,y+150-140,x+170-140,y+215-140);
-  line(x+170-140,y+215-140,x+105-140,y+215-140);
-  line(x+105-140,y+215-140,x+105-140,y+150-140);
-  line(x+125-140,y+170-140,x+60-140,y+170-140);
-  line(x+60-140,y+170-140,x+60-140,y+105-140);
-  line(x+60-140,y+105-140,x+125-140,y+105-140);
-  line(x+105-140,y+150-140,x+60-140,y+105-140);
-  line(x+60-140,y+105-140,x+105-140,y+60-140);
-  line(x+105-140,y+60-140,x+150-140,y+105-140);
-  line(x+125-140,y+105-140,x+170-140,y+60-140);
-  line(x+170-140,y+60-140,x+215-140,y+105-140);
-  line(x+215-140,y+105-140,x+170-140,y+150-140);
-  line(x+170-140,y+125-140,x+170-140,y+170-140);
-  line(x+215-140,y+170-140,x+170-140,y+215-140);
-  line(x+170-140,y+215-140,x+125-140,y+170-140);
-  line(x+150-140,y+170-140,x+105-140,y+215-140);
-  line(x+105-140,y+215-140,x+60-140,y+170-140);
-  line(x+60-140,y+170-140,x+105-140,y+125-140);
-  pop();
-}
-function hypercube(x,y) {
-hypercube_1(x+2.5,y+2.5);
+
+class Cloud {
+
+	constructor(x,y){
+    		this.x = x;
+        this.y = y;
+
+	}
+	drawCloud(){
+    		noStroke();
+    		fill(255);
+		    ellipse(this.x, this.y, 30, 30);
+        ellipse(this.x + 25, this.y, 30, 30);
+        ellipse(this.x + 10, this.y - 15, 30, 30);
+	}
+  moveCloud(){
+    this.x = this.x + 0.5
+  }
 }
