bd3dda56a9d0fa0a484ffb741b388011c3cbb2cf
KaiMcLister <71454898+KaiMcLister@users.noreply.github.com>
2020-09-18 10:10:16 -0700

    Update sketch.js

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -9,7 +9,7 @@ function setup() {
 }
 
 function draw(){
-	background(220);
+	background(255);
 
 //	draw all the balls in that array
 	for (let i = 0; i < balls.length; i++) {
@@ -19,28 +19,30 @@ function draw(){
 }
 
 function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-  let  b = new Ball(random(0,800), random(0,400));
+  let  b = new Ball(random(0,800), random(0,400), random(10,100));
   balls.push(b);
-  console.log(balls);
+  print(balls);
 }
 
 //ball class from which to create new balls with similar properties.
 class Ball {
 
-	constructor(x,y){ //every ball needs an x value and a y value
+	constructor(x,y,size){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
+        this.size = size;
+
 	}
 
 	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
+    		noStroke();
     		fill(random(0,270), random(0,270), random(0,270));
-		    ellipse(this.x,this.y,10,10);
+		    ellipse(this.x,this.y,this.size,this.size);
 	}
 
 	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+random(-2,2);
-		this.y = this.y+random(-0.5,0.5);
+		this.x = this.x+random(-10,10);
+		this.y = this.y+random(-5,5);
 	}
 
 
